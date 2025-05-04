fe0ac2b65030eed16f6c82167f9385e4212d221c
Vinicius <bossperson6@gmail.com>
2025-05-04 15:40:48 -0400

    Fix: remove experimental pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 import withPWA from "next-pwa";
 
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-    turbo: false,
-  },
   reactStrictMode: true,
+  experimental: {
+    turbo: false,
+    serverActions: true,
+  },
 };
 
-export default withPWA(nextConfig);
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // ✅ OK
+  // ❌ NÃO coloque 'experimental' aqui
+})(nextConfig);
