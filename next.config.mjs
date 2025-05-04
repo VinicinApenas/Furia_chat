9b99b4706f4ff466ec3d61760cec874d357682fa
Vinicius <bossperson6@gmail.com>
2025-05-04 17:52:26 -0400

    fix: ajusta configuração do next-pwa para deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
+// next.config.mjs
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   reactStrictMode: true,
   experimental: {
     turbo: false,
@@ -7,4 +10,9 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+const pwaConfig = {
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+};
+
+export default withPWA(pwaConfig)(baseConfig);
